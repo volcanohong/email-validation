5ef042c2efec04ebfa34cb83b291cf18f774b32b
volcanohong <volcanohong@gmail.com>
2018-05-08 15:42:05 -0700

    email auto complete - 1st version

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React, {Component} from 'react';
 import './App.css';
 import './AppConstants'
+import {Trie} from './trie';
 import * as AppConstant from './AppConstants';
+import {EMAIL_PROVIDERS} from "./AppConstants";
 
 class App extends Component {
+
+    dict = new Trie();
+
     constructor(props) {
         super(props);
         this.state = {email: '', error: ''};
         this.handleChange = this.handleChange.bind(this);
         this.handleKeyPress = this.handleKeyPress.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        EMAIL_PROVIDERS.forEach(email => this.dict.add(email));
     }
 
     validation() {
@@ -17,10 +23,12 @@ class App extends Component {
         if (!email) return this.error("email cannot be empty");
         if (typeof email !== "undefined") {
             let lastAtPos = email.lastIndexOf('@');
+            let lastDotPos = email.lastIndexOf('.');
             if (lastAtPos < 0) return this.error("missing '@'");
             let domainPart = email.substr(lastAtPos + 1);
             let domainRegExp = new RegExp(AppConstant.PATTERN_DOMAIN);
             if (!domainRegExp.test(domainPart)) return this.error("invalid domain name");
+            if (lastDotPos < lastAtPos) return this.error("missing '.' in domain");
             let namePart = email.substr(0, lastAtPos);
             let nameRegExp = new RegExp(AppConstant.PATTERN_NAME);
             if (!nameRegExp.test(namePart)) return this.error("is it a typo?");
@@ -38,8 +46,6 @@ class App extends Component {
         e.preventDefault();
         if (this.validation()) {
             this.setState({error: ""});
-        } else {
-
         }
     }
 
@@ -48,14 +54,29 @@ class App extends Component {
     }
 
     handleKeyPress(e) {
-        this.setState({email: e.target.value});
+        // this.setState({email: e.target.value});
         let keyCode = e.keyCode ? e.keyCode : e.which ? e.which : e.charCode;
-        if (AppConstant.KEYCODE_PROTECTED.indexOf(keyCode) >= 0) {
-            return;
-        }
-        // e.preventDefault();
+        if (AppConstant.KEYCODE_PROTECTED.indexOf(keyCode) >= 0) return;
 
-        console.log(String.fromCharCode(keyCode));
+        let atPos = e.target.value.indexOf('@');
+        if (atPos < 0) return;
+        let input = document.getElementById('email');
+        let start = e.target.selectionStart;
+        let name = e.target.value.substring(0, atPos + 1);
+        let prefix = e.target.value.substring(atPos + 1, start) + String.fromCharCode(keyCode);
+        let words = this.dict.getWordsByPrefix(prefix);
+
+        e.preventDefault();
+
+        if (words.length > 0) {
+            if (prefix.length === words[0].length) return;
+            input.value = name + words[0];
+            setTimeout(function () {
+                input.setSelectionRange(start + 1, start + words[0].length);
+            }, 0);
+        } else {
+            input.value = name + prefix;
+        }
     }
 
     render() {
@@ -74,5 +95,4 @@ class App extends Component {
         );
     }
 }
-
 export default App;
